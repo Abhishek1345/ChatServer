b45eb9e7f18b77fa316f3636e4c25454a42391fa
Abhishek <jawanpuriaabhishek@gmail.com>
2023-09-08 20:15:30 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+import { WebSocketServer } from "ws";
+const port=process.env.PORT || 3000
+const server = new WebSocketServer({ port: port});
+
+server.on("connection", (socket) => {
+  //send a message to the client
+//cons-ole.log("connection established");
+// setInterval(()=>{
+//   socket.send(JSON.stringify({
+//     type: "test message from server",
+//     msg:"hello",
+//    content: [ 1, "2" ]
+//    }));
+// },2000);
+
+  // receive a message from the client
+  let sendMessage=function(pkt){
+    server.clients.forEach((client)=>{
+      client.send(JSON.stringify({
+        type:"message",
+        clientId:pkt.clientId,
+        msg:pkt.msg
+      }));
+    
+   
+  });
+}
+  
+  socket.on("message", (data) => {
+    
+    const packet = JSON.parse(data);
+    console.log("message recieved");
+    sendMessage(packet);
+    console.log("message sent");
+  });
+});
